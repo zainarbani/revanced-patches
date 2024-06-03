3bb027691a63a7536da2a2e4d57b1ae80b38d084
zainarbani <zaintsyariev@gmail.com>
2024-06-03 12:44:58 +0700

    fix(yt): zzz2

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/BuildVideoPlaybackConnectionFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/BuildVideoPlaybackConnectionFingerprint.kt
@@ -1,10 +1,15 @@
 package app.revanced.patches.youtube.misc.fix.playback.fingerprints
 
 import app.revanced.patcher.fingerprint.MethodFingerprint
-import com.android.tools.smali.dexlib2.Opcode
+import com.android.tools.smali.dexlib2.AccessFlags
 
 internal object BuildVideoPlaybackConnectionFingerprint : MethodFingerprint(
+    accessFlags = AccessFlags.PRIVATE or AccessFlags.FINAL,
     returnType = "Ljava/net/HttpURLConnection;",
+    parameters = listOf(
+        "Ljava/net/URL;", // videoplayback URL
+        "I",
+    ),
     strings = listOf(
         "Range",
         "User-Agent",
