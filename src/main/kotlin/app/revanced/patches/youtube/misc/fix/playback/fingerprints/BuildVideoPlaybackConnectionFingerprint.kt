866ec13da26ff2a38517ac76445d419393f6f927
zainarbani <zaintsyariev@gmail.com>
2024-06-03 13:09:52 +0700

    fix(yt): zzz5

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/BuildVideoPlaybackConnectionFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/BuildVideoPlaybackConnectionFingerprint.kt
@@ -3,6 +3,7 @@ package app.revanced.patches.youtube.misc.fix.playback.fingerprints
 import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.MethodFingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
+import com.android.tools.smali.dexlib2.Opcode
 
 internal object BuildVideoPlaybackConnectionFingerprint : MethodFingerprint(
     accessFlags = AccessFlags.PRIVATE or AccessFlags.FINAL,
@@ -17,11 +18,12 @@ internal object BuildVideoPlaybackConnectionFingerprint : MethodFingerprint(
         "Z",
         "Ljava/util/Map;",
     ),
+    opcodes = listOf(
+        Opcode.INVOKE_VIRTUAL,
+        Opcode.MOVE_RESULT_OBJECT,
+    ),
     strings = listOf(
-        "Range",
-        "User-Agent",
         "identity",
         "gzip",
-        "Accept-Encoding",
     ),
 )
