1cea7bfe35927b11df92e622c0a016e370d1950d
zainarbani <zaintsyariev@gmail.com>
2024-06-06 23:11:17 +0700

    fix(yt): kkn3

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/BuildPlayerRequestFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/BuildPlayerRequestFingerprint.kt
@@ -25,6 +25,10 @@ internal object BuildPlayerRequestFingerprint : MethodFingerprint(
         "Z",
         "L",
     ),
+    opcodes = listOf(
+        Opcode.INVOKE_STATIC,
+        Opcode.MOVE_RESULT_OBJECT,
+    ),
     customFingerprint = { methodDef, classDef ->
         methodDef.name == "<init>" &&
         classDef.type.endsWith("FormatStreamModel;")
