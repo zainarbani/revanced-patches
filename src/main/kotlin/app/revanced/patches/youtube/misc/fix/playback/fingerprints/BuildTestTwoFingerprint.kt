4bec0c5cb9fc675a5559b030799b39c49f5579d0
zainarbani <zaintsyariev@gmail.com>
2024-06-11 10:51:26 +0700

    fix(yt): dda4

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/BuildTestTwoFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/BuildTestTwoFingerprint.kt
@@ -1,5 +1,6 @@
 package app.revanced.patches.youtube.misc.fix.playback.fingerprints
 
+import app.revanced.patches.youtube.misc.fix.playback.fingerprints.BuildTestTwoFingerprint.constWideIndex
 import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.MethodFingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
@@ -10,9 +11,12 @@ internal object BuildTestTwoFingerprint : MethodFingerprint(
     accessFlags = AccessFlags.PUBLIC or AccessFlags.FINAL,
     returnType = "Z",
     customFingerprint = { methodDef, _ ->
+        constWideIndex(methodDef) >= 0
+    }
+) {
+    private fun constWideIndex(methodDef: Method) =
         methodDef.indexOfFirstInstruction {
             opcode == Opcode.CONST_WIDE_32 &&
             literal == 0x2b46463L
-        } >= 0
-    }
-)
+        }
+}
