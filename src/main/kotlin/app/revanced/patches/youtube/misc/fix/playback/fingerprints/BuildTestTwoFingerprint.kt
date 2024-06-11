613b29b86dc333121a5dda8abc029d11e1b03695
zainarbani <zaintsyariev@gmail.com>
2024-06-11 11:17:14 +0700

    fix(yt): ddj

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/BuildTestTwoFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/BuildTestTwoFingerprint.kt
@@ -1,23 +1,21 @@
 package app.revanced.patches.youtube.misc.fix.playback.fingerprints
 
-import app.revanced.patches.youtube.misc.fix.playback.fingerprints.BuildTestTwoFingerprint.constWideIndex
-import app.revanced.util.indexOfFirstInstruction
+//import app.revanced.patches.youtube.misc.fix.playback.fingerprints.BuildTestTwoFingerprint.constWideIndex
+//import app.revanced.util.indexOfFirstInstruction
 import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.MethodFingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
-import com.android.tools.smali.dexlib2.iface.Method
+//import com.android.tools.smali.dexlib2.iface.Method
 
 internal object BuildTestTwoFingerprint : MethodFingerprint(
     accessFlags = AccessFlags.PUBLIC or AccessFlags.FINAL,
     returnType = "Z",
     customFingerprint = { methodDef, _ ->
-        constWideIndex(methodDef) >= 0
+        methodDef.implementation?.instructions?.any { instruction ->
+            if (instruction.opcode != Opcode.CONST_WIDE_32) return@any false
+            if (instruction.literal != 0x2b46463L) return@any false
+            true
+         } ?: false
     }
-) {
-    fun constWideIndex(methodDef: Method) =
-        methodDef.indexOfFirstInstruction {
-            opcode == Opcode.CONST_WIDE_32 &&
-            literal == 0x2b46463L
-        }
-}
+)
