84711f476b6e196f16b49864ce346c0722aca51e
zainarbani <zaintsyariev@gmail.com>
2024-06-15 23:41:19 +0700

    fix(yt): ddb

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/BuildTestTwoFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/BuildTestTwoFingerprint.kt
@@ -3,6 +3,7 @@ package app.revanced.patches.youtube.misc.fix.playback.fingerprints
 import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.MethodFingerprint
 import com.android.tools.smali.dexlib2.AccessFlags
+import com.android.tools.smali.dexlib2.Opcode
 
 internal object BuildTestTwoFingerprint : MethodFingerprint(
     accessFlags = AccessFlags.PROTECTED.value,
