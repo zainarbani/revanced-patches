d7e4a8222904742246d4011155a549eeac053cc2
zainarbani <zaintsyariev@gmail.com>
2024-06-16 19:07:19 +0700

    fix(yt): ddc7

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/BuildTestTwoFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/BuildTestTwoFingerprint.kt
@@ -20,4 +20,12 @@ internal object BuildTestTwoFingerprint : MethodFingerprint(
         "I",
         "Ljava/lang/Object;",
     ),
+    opcodes = listOf(
+        Opcode.INVOKE_STATIC,
+        Opcode.MOVE_OBJECT,
+        Opcode.IPUT_OBJECT, // stream uri.
+    ),
+    customFingerprint = { methodDef, _ ->
+        methodDef.name == "<init>"
+    },
 )
