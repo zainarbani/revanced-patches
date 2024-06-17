cebd1d9c3fba30356fc305a654038f064aeee563
zainarbani <zaintsyariev@gmail.com>
2024-06-17 08:17:27 +0700

    fix(yt): ddy2

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/BuildTestTwoFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/BuildTestTwoFingerprint.kt
@@ -8,17 +8,10 @@ import com.android.tools.smali.dexlib2.Opcode
 internal object BuildTestTwoFingerprint : MethodFingerprint(
     accessFlags = AccessFlags.PUBLIC or AccessFlags.CONSTRUCTOR,
     returnType = "V",
-    parameters = listOf(
-        "Landroid/net/Uri;",
-        "J",
-        "I",
-        "[B",
-        "Ljava/util/Map;",
-        "J",
-        "J",
-        "Ljava/lang/String;",
-        "I",
-        "Ljava/lang/Object;"
+    parameterCount = 10,
+    opcodes = listOf(
+        Opcode.IPUT_OBJECT,
+        Opcode.IPUT_WIDE
     ),
     customFingerprint = { methodDef, classDef ->
         methodDef.name == "<init>" &&
