4d2d9eba39b99f426676b6870e8ce14d3767fecb
oSumAtrIX <johan.melkonyan1@web.de>
2024-08-01 13:52:11 +0200

    chore: Merge integrations

--- patches/src/main/kotlin/app/revanced/generator/Main.kt
+++ patches/src/main/kotlin/app/revanced/generator/Main.kt
@@ -4,7 +4,7 @@ import app.revanced.patcher.patch.loadPatchesFromJar
 import java.io.File
 
 internal fun main() = loadPatchesFromJar(
-    setOf(File("build/libs/").listFiles { it -> it.name.endsWith(".rvp") }!!.first()),
+    setOf(File("patches/build/libs/").listFiles { it -> it.name.endsWith(".rvp") }!!.first()),
 ).also { loader ->
     if (loader.isEmpty()) throw IllegalStateException("No patches found")
 }.let { bundle ->
