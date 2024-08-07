d2fe20cf23e0c579b048af182faff46d0819c5b7
zainarbani <zaintsyariev@gmail.com>
2024-08-07 22:03:33 +0700

    fix(yt): test7

--- src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/TestFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/misc/fix/playback/fingerprints/TestFingerprint.kt
@@ -8,21 +8,24 @@ import com.android.tools.smali.dexlib2.Opcode
 internal object TestFingerprint : MethodFingerprint(
     accessFlags = AccessFlags.PUBLIC or AccessFlags.CONSTRUCTOR,
     returnType = "V",
-    //opcodes = listOf(
-   //     Opcode.MOVE_OBJECT
-   //     Opcode.MOVE_WIDE
-    //    Opcode.MOVE_OBJECT_FROM16,
- //       Opcode.MOVE_WIDE_FROM16,
-  //      Opcode.INVOKE_DIRECT
-  //  ),
-    customFingerprint = custom@{ methodDef, _ ->
-        if (methodDef.name != "<init>") return@custom false
-        
-        val parameterTypes = methodDef.parameterTypes
-        
-        if (parameterTypes.size != 10) return@custom false
-        if (parameterTypes[0] != "Landroid/net/Uri;") return@custom false
-        if (parameterTypes[1] != "J") return@custom false
-        return@custom true
+    parameters = listOf(
+        "Landroid/net/Uri;",
+        "J",
+        "I",
+        "[B",
+        "Ljava/util/Map;",
+        "J",
+        "J",
+        "Ljava/lang/String;",
+        "I",
+        "Ljava/lang/Object;"
+    ),
+    opcodes = listOf(
+        Opcode.MOVE_OBJECT_FROM16,
+        Opcode.MOVE_WIDE_FROM16,
+        Opcode.INVOKE_DIRECT
+    ),
+    customFingerprint = { methodDef, _ ->
+        methodDef.name == "<init>" && methodDef.annotations.isEmpty()
     }
 )
