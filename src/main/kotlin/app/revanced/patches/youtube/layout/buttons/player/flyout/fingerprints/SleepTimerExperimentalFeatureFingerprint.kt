fe7e5b94b8839cdf3733a5b0b0579e4c2fbf261f
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-09-24 03:02:50 -0400

    refactor: Add more checks to fingerprints

--- src/main/kotlin/app/revanced/patches/youtube/layout/buttons/player/flyout/fingerprints/SleepTimerExperimentalFeatureFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/buttons/player/flyout/fingerprints/SleepTimerExperimentalFeatureFingerprint.kt
@@ -1,9 +1,12 @@
 package app.revanced.patches.youtube.layout.buttons.player.flyout.fingerprints
 
+import app.revanced.patcher.extensions.or
 import app.revanced.util.patch.LiteralValueFingerprint
+import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
 
 internal object SleepTimerExperimentalFeatureFingerprint : LiteralValueFingerprint(
+    accessFlags = AccessFlags.PUBLIC or AccessFlags.CONSTRUCTOR,
     opcodes = listOf(
         Opcode.IPUT_BOOLEAN,
         Opcode.RETURN_VOID
