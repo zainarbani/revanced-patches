e3d360f41a2a81e9c4372d29522d44f8a0cd864e
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2024-09-24 03:10:31 -0400

    refactor: simplify

--- src/main/kotlin/app/revanced/patches/youtube/layout/buttons/player/flyout/SleepTimerMenuPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/buttons/player/flyout/SleepTimerMenuPatch.kt
@@ -2,13 +2,13 @@ package app.revanced.patches.youtube.layout.buttons.player.flyout
 
 import app.revanced.patcher.data.BytecodeContext
 import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
-import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.youtube.layout.buttons.player.flyout.fingerprints.SleepTimerFeatureFingerprint
 import app.revanced.patches.youtube.layout.buttons.player.flyout.fingerprints.SleepTimerFlyoutMenuFeatureFingerprint
 import app.revanced.util.resultOrThrow
+import app.revanced.util.returnEarly
 
 @Patch(
     name = "Remove sleep timer menu restrictions",
@@ -27,13 +27,7 @@ object SleepTimerMenuPatch : BytecodePatch(
     setOf(SleepTimerFeatureFingerprint, SleepTimerFlyoutMenuFeatureFingerprint)
 ) {
     override fun execute(context: BytecodeContext) {
-        SleepTimerFeatureFingerprint.resultOrThrow().mutableMethod.addInstructions(
-            0,
-            """
-                const/4 v0, 0x1
-                return v0
-            """
-        )
+        SleepTimerFeatureFingerprint.returnEarly(true)
 
         // Feature is experimental, and this fingerprint may change in the future.
         SleepTimerFlyoutMenuFeatureFingerprint.resultOrThrow().let {
