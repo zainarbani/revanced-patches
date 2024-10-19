4fa3ac7c6473b6df660ed1eba428ccf8f47b5ed2
zainarbani <zaintsyariev@gmail.com>
2024-10-20 00:05:27 +0700

    fix: Remove duplicate menu

--- src/main/kotlin/app/revanced/patches/youtube/layout/buttons/player/flyout/SleepTimerMenuPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/buttons/player/flyout/SleepTimerMenuPatch.kt
@@ -1,13 +1,10 @@
 package app.revanced.patches.youtube.layout.buttons.player.flyout
 
 import app.revanced.patcher.data.BytecodeContext
-import app.revanced.patcher.extensions.InstructionExtensions.addInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.CompatiblePackage
 import app.revanced.patcher.patch.annotation.Patch
 import app.revanced.patches.youtube.layout.buttons.player.flyout.fingerprints.SleepTimerFeatureFingerprint
-import app.revanced.patches.youtube.layout.buttons.player.flyout.fingerprints.SleepTimerFlyoutMenuFeatureFingerprint
-import app.revanced.util.resultOrThrow
 import app.revanced.util.returnEarly
 
 @Patch(
@@ -17,27 +14,16 @@ import app.revanced.util.returnEarly
         CompatiblePackage(
             "com.google.android.youtube",
             [
-                "19.30.39", // Only enable this patch on 19.30+ for now.
+                "19.34.42", // Only enable this patch on 19.30+ for now.
             ]
         )
     ]
 )
 @Suppress("unused")
 object SleepTimerMenuPatch : BytecodePatch(
-    setOf(SleepTimerFeatureFingerprint, SleepTimerFlyoutMenuFeatureFingerprint)
+    setOf(SleepTimerFeatureFingerprint)
 ) {
     override fun execute(context: BytecodeContext) {
         SleepTimerFeatureFingerprint.returnEarly(true)
-
-        // Feature is experimental, and this fingerprint may change in the future.
-        SleepTimerFlyoutMenuFeatureFingerprint.resultOrThrow().let {
-            it.mutableMethod.apply {
-                val targetIndex = it.scanResult.patternScanResult!!.startIndex
-
-                addInstruction(
-                    targetIndex, "const/4 p1, 0x1"
-                )
-            }
-        }
     }
 }
